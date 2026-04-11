2b9e4f8c72a27c5d69fc8b653e72837f2912255f
anaalexandrinadumitriu-crypto <ana.alexandrina.dumitriu@gmail.com>
2026-04-11 21:39:44 +0000

    Add logo to the main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("FeedME")
+col1, col2, col3 = st.columns(3)
+with col2:
+    st.image("Logo.png", width=200)
+    st.markdown("<div style='text-align: center; font-size: xx-large; font-weight: bold;'>FeedME</div>", unsafe_allow_html=True)
 st.write(
-    "What ingredients do you have?"
+    "What ingredients do you have today?"
 )
