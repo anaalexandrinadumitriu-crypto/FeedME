d5eeee914213086e1f69df2cf9a0b8289f3f3bc8
anaalexandrinadumitriu-crypto <ana.alexandrina.dumitriu@gmail.com>
2026-04-20 14:57:28 +0000

    Remove unncecessary text

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,7 @@ if st.session_state.page == 'home':
     with col2:
         st.image("Logo.png", width=200)
         st.markdown("<div style='text-align: center; font-size: xx-large; font-weight: bold;'>FeedME</div>", unsafe_allow_html=True)
-    st.write("What ingredients do you have today?")
-
+   
 # Feed Me page
 elif st.session_state.page == 'feed_me':
     st.header("🍽️ Feed Me")
