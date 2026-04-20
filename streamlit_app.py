bc3054fa55e7579e75ff123340a51aedc2d63125
anaalexandrinadumitriu-crypto <ana.alexandrina.dumitriu@gmail.com>
2026-04-20 18:12:04 +0000

    Change logo size

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,9 @@ with col3:
 if st.session_state.page == 'home':
     col1, col2, col3 = st.columns(3)
     with col2:
+        st.markdown("<div style='text-align: center'>", unsafe_allow_html=True)
         st.image("Logo.png", width=400)
+        st.markdown("</div>", unsafe_allow_html=True)
         st.markdown("<div style='text-align: center; font-size: xx-large; font-weight: bold;'>FeedME</div>", unsafe_allow_html=True)
    
 # Feed Me page
