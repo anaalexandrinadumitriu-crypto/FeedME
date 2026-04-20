3fd2ddfb52102bb3adeda7a3a9656b43190aa187
anaalexandrinadumitriu-crypto <ana.alexandrina.dumitriu@gmail.com>
2026-04-20 18:34:07 +0000

    Center Logo image

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,7 @@ with col3:
 if st.session_state.page == 'home':
     col1, col2, col3 = st.columns(3)
     with col2:
-        st.markdown("<div style='text-align: center'>", unsafe_allow_html=True)
-        st.image("Logo.png", width=400)
-        st.markdown("</div>", unsafe_allow_html=True)
+        st.image("Logo.png", width=1000)
         st.markdown("<div style='text-align: center; font-size: 60px; font-weight: bold;'>FeedME</div>", unsafe_allow_html=True)
    
 # Feed Me page
