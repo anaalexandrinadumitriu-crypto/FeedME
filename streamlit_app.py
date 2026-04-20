6d5944e77d61718c9d5618c8a1d59b7bb38bb156
anaalexandrinadumitriu-crypto <ana.alexandrina.dumitriu@gmail.com>
2026-04-20 18:22:28 +0000

    Make front page text larger

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if st.session_state.page == 'home':
         st.markdown("<div style='text-align: center'>", unsafe_allow_html=True)
         st.image("Logo.png", width=400)
         st.markdown("</div>", unsafe_allow_html=True)
-        st.markdown("<div style='text-align: center; font-size: xx-large; font-weight: bold;'>FeedME</div>", unsafe_allow_html=True)
+        st.markdown("<div style='text-align: center; font-size: 60px; font-weight: bold;'>FeedME</div>", unsafe_allow_html=True)
    
 # Feed Me page
 elif st.session_state.page == 'feed_me':
